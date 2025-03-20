bf1b1c5790d799a8f6b5af5afae0e59df9aaa95e
413f10r <ale-fioretti@live.it>
2025-03-20 03:13:40 +0100

    Add: milestone 4

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,50 @@
+; import MovieCard from '../components/MovieCard'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+export default function HomePage() {
+
+
+    const { movies, setMovies } = useState([]);
+
+    //fetchIndex for movies
+    const fetchMovies = () => {
+        console.log('Fetching movies...');
+
+        axios
+            .get('http://localhost:3000/movies')
+            .then((res) => {
+                setMovies(res.data)
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    //funzione per rendering delle card dei libri nell'html
+    const renderMovie = () => {
+        return movies.map((movie) => {
+            return (
+                <div className="col" key={movie.id}>
+                    <MovieCard movie={movie} />
+                </div>
+            );
+        });
+    }
+
+    //Invocazione chiamata al caricamento del componente in pagina
+    useEffect(fetchMovies, []);
+    return (
+        <>
+            <h1 className="text-primary">BoolMovie</h1>
+            <h2 className="row row-cols-3">Movies list</h2>
+            <div className="row row-cols-3">
+
+                {renderMovie()}
+            </div>
+        </>
+    )
+
+
+    
+}
---
+++ src/pages/MoviePage.jsx
@@ -0,0 +1,47 @@
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+//uso del compoennte ReviewCard
+import ReviewCard from '../components/ReviewCard';
+
+export default function MoviePage() {
+    //Estrazione dinformazione dall'url del sito
+    //http:localhost:3000/movie/2
+    const { id } = useParams(); //2
+
+    //Crreazione varibaile reattiva
+    const [movie, setMovie] = useState({});
+
+    //Chiamata fetch Show
+    const fetchMovie = () => {
+        axios
+            .get(`http://localhost:3000/mocvies/${id}`)
+            .then((res) => setMovie(res.data))
+            .catch((error) => console.log(error));
+    };
+
+    //Invocazione chiamata al caricamento del componente in pagina
+    useEffect(fetchMovie, [id]);
+
+    //Rendering html delle reviews
+    const renderReviews = () => {
+        return movie.reviews?.map((review) => {
+            return <ReviewCard key={review.id} review={review} />;
+        });
+    };
+
+    return (
+        <>
+            <h1>{movie?.title}</h1>
+            <img src={movie?.image} alt={movie?.title} />
+            {/* qui andrà la pagina di dettaglio del prodotto */}
+
+            <section>
+                <h4>Our community reviews</h4>
+                {renderReviews()}
+            </section>
+        </>
+    );
+}
